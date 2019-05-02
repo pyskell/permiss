6a3519bef4c05d81b1e39405586cb456a9d651aa
Anthony <pyskell@users.noreply.github.com>
2019-05-02 13:37:15 -0400

    update .gitignore; minor readability tweaks

--- contracts/PermissAbstract.sol
+++ contracts/PermissAbstract.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.21 <0.6.0;
-
 import "./ChildContract.sol";
 
+// These contracts should *never* hold state beyond the small bit specified
 contract PermissAbstract{
     bool enabled = true;
     address upgrade_address = address(0x0);
@@ -17,12 +17,12 @@ contract PermissAbstract{
     // But a manager has sole control over whether or not the contract is enabled.
     // Most of the modifiers below handle times when permissions for each function are different.
     modifier notUpgraded {
-        require(upgrade_address == address(0x0), "Cannot upgrade a contract more than once");
+        require(upgrade_address == address(0x0), "Contract already upgraded");
         _;
     }
     modifier enabledContract{
         // Prevents someone pointing a disabled contract to a new address under their control.
-        require(enabled == true, "Can only upgrade a contract that is enabled");
+        require(enabled == true, "Contract must be enabled");
         _;
     }
     modifier uniqueUpgradeAddress{
@@ -34,7 +34,11 @@ contract PermissAbstract{
     function permitted(bytes32[] calldata _permission) external enabledContract returns(bool){
         child.permitted(_permission);
     }
-    function upgrade(bytes32[] calldata _permission) external;
+    function upgrade(bytes32[] calldata _permission) external notUpgraded enabledContract uniqueUpgradeAddress {
+        child.upgrade(_permission);
+    }
     function enable(bytes32[] calldata _permission) external;
-    function disable(bytes32[] calldata _permission) external;
+    function disable(bytes32[] calldata _permission) external enabledContract{
+        child.disable(_permission);
+    }
 }
