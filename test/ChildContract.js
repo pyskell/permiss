c4556e7e250711067a93651fa8a4a5dbef9fe80a
Anthony <pyskell@users.noreply.github.com>
2019-05-07 10:20:18 -0400

    broke the tests but this is better than broken tests that were passing

--- test/ChildContract.js
+++ test/ChildContract.js
@@ -1,10 +1,20 @@
 const ChildContract = artifacts.require("ChildContract");
 
 contract("ChildContract", accounts => {
-    it("should return true when called by the contract owner", () => 
-        ChildContract.new(accounts[0])
+    it("should return true when called by the contract owner", () => {
+        let permitted;
+        ChildContract.new(accounts[0]) // The contract owner.
         .then(instance =>{
-            assert(instance.permitted(), true);
+            permitted = instance.permitted.call([]);
         })
-    );
+        assert(permitted, true);
+    });
+    it("should throw when called by another address", () => {
+        let permitted;
+        ChildContract.new(accounts[1]) // Not the contract owner.
+        .then(instance =>{
+            permitted = instance.permitted.call([]);
+        })
+        assert.instanceOf(permitted, Error);
+    });
 })
